9c9b90fea3119e1196d0cbe3f7b484edfcc2f2ba
Kamran <kamran-anwar99@hotmail.com>
2022-03-25 17:40:14 +0000

    UI alerts, validation check, remove book

--- app.js
+++ app.js
@@ -44,6 +44,24 @@ class UI {
 
     }
 
+    static deleteBook(el) {
+        if (el.classList.contains('delete')) {
+            el.parentElement.parentElement.remove(0);
+        }
+    }
+
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = ` alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+
+        //Alert goes after 2 seconds
+        setTimeout(() => document.querySelector('.alert').remove(), 2000);
+    }
+
     static clearFields() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
@@ -65,14 +83,29 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
+    //Validate
+    if (title === '' || author === '' || isbn === '') {
+        UI.showAlert('Please fill in all fields', 'danger'); //Should go away after few seconds
+    } else {
 
-    //Instantiate book
-    const book = new Book(title, author, isbn);
+        //Instantiate book
+        const book = new Book(title, author, isbn);
 
-    //Add book to UI
-    UI.addBookToList(book);
+        //Add book to UI
+        UI.addBookToList(book);
 
-    // CLEAR FIELDS
-    UI.clearFields();
+        // Book added successfully
+        UI.showAlert('Book Added', 'success');
+
+        // CLEAR Fields
+        UI.clearFields();
+    }
 });
 // Event: Remove a book
+document.querySelector('#book-list').addEventListener('click', (e) => { //Targets the list
+    UI.deleteBook(e.target);
+
+
+    // Book added successfully
+    UI.showAlert('Book Removed', 'info');
+});
